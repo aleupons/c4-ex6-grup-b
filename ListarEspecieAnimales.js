d398342ea88e6fe82fcaa34b6b58b428958425ff
aleupons <apahoquei@gmail.com>
2021-07-07 12:45:37 +0200

    Capturar errors al fer funcions que consulten BBDD

--- ListarEspecieAnimales.js
+++ ListarEspecieAnimales.js
@@ -2,26 +2,30 @@ const Especie = require("./schemas/Especie");
 const Animal = require("./schemas/Animal");
 
 const listarEspecieAnimales = async (nombreEspecie) => {
-  const animales = await Animal.findAll({
-    include: {
-      model: Especie,
-      required: true,
-    },
-    order: [
-      ["especie", "DESC"],
-      ["nombre", "DESC"],
-    ],
-  });
-  console.log(`\nAnimales de la especie ${nombreEspecie}:`);
-  let comptador = 0;
-  for (const animal of animales) {
-    if (nombreEspecie.toLowerCase() === animal.Especie.nombre.toLowerCase()) {
-      console.log(animal.nombre);
-      comptador++;
+  try {
+    const animales = await Animal.findAll({
+      include: {
+        model: Especie,
+        required: true,
+      },
+      order: [
+        ["especie", "DESC"],
+        ["nombre", "DESC"],
+      ],
+    });
+    console.log(`\nAnimales de la especie ${nombreEspecie}:`);
+    let comptador = 0;
+    for (const animal of animales) {
+      if (nombreEspecie.toLowerCase() === animal.Especie.nombre.toLowerCase()) {
+        console.log(animal.nombre);
+        comptador++;
+      }
     }
-  }
-  if (!comptador) {
-    console.log(`No tienes animales de la especie ${nombreEspecie}`);
+    if (!comptador) {
+      console.log(`No tienes animales de la especie ${nombreEspecie}`);
+    }
+  } catch (error) {
+    console.log("No tenemos especies de animales");
   }
 };
 
--- listarAnimales.js
+++ listarAnimales.js
@@ -2,18 +2,22 @@ const Animal = require("./schemas/Animal");
 const Especie = require("./schemas/Especie");
 
 const listarAnimales = async () => {
-  const animales = await Animal.findAll({
-    include: { model: Especie },
-    order: [
-      ["especie", "DESC"],
-      ["nombre", "DESC"],
-    ],
-  });
-  console.log("\nAnimales por nombre y especie:");
-  for (const animal of animales) {
-    console.log(
-      `${animal.nombre} -> Edad: ${animal.edad} Especie: ${animal.Especie.nombre}`
-    );
+  try {
+    const animales = await Animal.findAll({
+      include: { model: Especie },
+      order: [
+        ["especie", "DESC"],
+        ["nombre", "DESC"],
+      ],
+    });
+    console.log("\nAnimales por nombre y especie:");
+    for (const animal of animales) {
+      console.log(
+        `${animal.nombre} -> Edad: ${animal.edad} Especie: ${animal.Especie.nombre}`
+      );
+    }
+  } catch (error) {
+    console.log("No tenemos animales");
   }
 };
 
