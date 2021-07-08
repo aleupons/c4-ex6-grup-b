6a9a4cbd6544ef71c7de39aa33ba6b320ee91d37
Pol Reig <polreigbroto@gmail.com>
2021-07-08 10:10:07 +0200

    Error general y error404 declarados

--- servidor/index.js
+++ servidor/index.js
@@ -1,9 +1,13 @@
 const morgan = require("morgan");
 const express = require("express");
 const app = require("./init");
+const { error404, errorGeneral } = require("./errores");
 const rutasAnimales = require("./rutas/animales");
 
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/alumnos", rutasAnimales);
+
+app.use(error404);
+app.use(errorGeneral);
